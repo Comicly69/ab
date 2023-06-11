e404972d12ceb1f34f4ccb472a8cd71267504692
Comicly69 <snowyoumc@gmail.com>
2023-06-10 20:22:43 -0400

    add installer

---
+++ install.sh
@@ -0,0 +1,5 @@
+echo Installing...
+wget https://github.com/Comicly69/about/raw/about-v1.1/console/bin/latest/about
+sudo mv about /usr/local/bin
+sudo chmod +x /usr/local/bin/about
+echo Finished install!
